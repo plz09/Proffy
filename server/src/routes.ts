b383cff7527ca305e6ecb25c0bd4dd7852cedd9f
plz09 <62223358+plz09@users.noreply.github.com>
2020-08-09 16:21:21 +0100

    Creating connections routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,6 +9,8 @@ const connectionsController = new ConnectionsController();
 
 routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
-routes.post('/connections', connectionsController.create)
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
